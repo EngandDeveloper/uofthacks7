895d3e89f11ffe29cd60f048d2063360033809b4
enganddeveloper <hackathon@gmail.com>
2020-01-18 02:54:17 -0500

    camera.js added

---
+++ js/camera.js
@@ -0,0 +1,82 @@
+//access to the camera
+//###################################################################3
+var videoElement = document.querySelector('video');
+var videoSelect = document.querySelector('select#videoSource');
+
+navigator.mediaDevices.enumerateDevices()
+  .then(gotDevices).then(getStream).catch(handleError);
+
+videoSelect.onchange = getStream;
+
+function gotDevices(deviceInfos) {
+  for (var i = deviceInfos.length - 1; i >= 0; --i) {
+    var deviceInfo = deviceInfos[i];
+    var option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' +
+        (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    } else {455
+      console.log('Found one other kind of source/device: ', deviceInfo);
+    }
+  }
+}
+
+function getStream() {
+  buttonGo.disabled = false;
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+  }
+
+  var constraints = {
+    video: {
+      deviceId: {exact: videoSelect.value}
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).
+    then(gotStream).catch(handleError);
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('Error: ', error);
+}
+//access to camera ends
+//#############################################################
+
+// draw image into canvas
+//#################################################
+var barcodeCanvas = document.createElement("canvas");
+barcodeCanvas.width = vid.videoWidth;
+barcodeCanvas.height = vid.videoHeight;
+var barcodeContext = barcodeCanvas.getContext('2d');
+var imageWidth = vid.videoWidth, imageHeight = vid.videoHeight;
+barcodeContext.drawImage(videoElement, 0, 0, imageWidth, imageHeight);
+//###################################################
+
+//get images data from canvas
+//####################################################
+var imageData = barcodeContext.getImageData(0, 0, imageWidth, imageHeight);
+var idd = imageData.data;
+//#########################################################
+
+//copy pixels to buffer
+//#########################################################
+var image = ZXing._resize(imageWidth, imageHeight);
+  for (var i = 0, j = 0; i < idd.length; i += 4, j++) {
+    ZXing.HEAPU8[image + j] = idd[i];
+  }
+//##########################################################
+
+//decode the barcode
+//######################################################
+var err = ZXing._decode_any(decodePtr);
+//#####################################################
